0166a83c513cf9d4f3ce015972d5757aceb4cad5
icimodel <94973964+icimodel@users.noreply.github.com>
2021-11-24 15:40:54 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ using Documenter, ICI_simulateur
 makedocs(
     modules = [ICI_simulateur],
     format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
-    authors = "Nicolas GILET and Maxime Colomb",
+    authors = "Nicolas GILET and Maxime COLOMB",
     sitename = "ICI_simulateur.jl",
     pages = Any["index.md"]
     # strict = true,
